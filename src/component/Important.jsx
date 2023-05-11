fd09f1b41aad8f0f226736af6a449b1c25dc8f0b
Matheus Chiodi <matheuschiodi20@gmail.com>
2023-05-11 17:46:35 -0300

    update ajuste Importante

--- src/component/Important.jsx
+++ src/component/Important.jsx
@@ -5,12 +5,16 @@ export default function Important() {
                 <p className="text-[30px] font-bold w-full text-center">
                     Important
                 </p>
-                <div className="grid grid-cols-2 w-full m-0 p-0">
+                <div className="grid grid-cols-1 md:grid-cols-2 w-full m-0 p-0">
                     <div className="bg-red h-[250px] me-2 rounded-[20px]">
 
                     </div>
                     <div className="bg-red h-[250px] ms-2 rounded-[20px]">
 
+                    </div>
+                </div>
+                <div className="w-full m-0 p-0">
+                    <div className="bg-red h-[250px] me-2 rounded-[20px]">
                     </div>
                 </div>
             </div>
