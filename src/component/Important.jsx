0185b5c7c5e3cda1a0eb63e49ed472363f8bb4d0
Matheus Chiodi <matheuschiodi20@gmail.com>
2023-05-11 17:53:55 -0300

    update ajuste Importante 4

--- src/component/Important.jsx
+++ src/component/Important.jsx
@@ -1,7 +1,7 @@
 export default function Important() {
     return (
         <>
-            <div className="w-full px-[76px] bg-[#f8f8f2] h-[300px] mt-[20px]">
+            <div className="w-full md:px-[76px] px-3 bg-[#f8f8f2] h-[300px] mt-[20px]">
                 <p className="text-[30px] font-bold w-full text-center">
                     Important
                 </p>
